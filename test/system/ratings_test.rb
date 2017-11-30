169b3fc27f7c2c0c2e6928cc8120431d7b3f8e65
Jakub <jakub94@gmx.de>
2017-11-30 19:53:07 +0100

    removed old test dir

--- test/system/ratings_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class RatingsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit ratings_url
-  #
-  #   assert_selector "h1", text: "Rating"
-  # end
-end
