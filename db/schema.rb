c5f6cda3f65c2fa664b0762f781dc837b578391c
Jakub <jakub94@gmx.de>
2017-12-07 21:11:23 +0100

    before pulling

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123103113) do
+ActiveRecord::Schema.define(version: 20171207200458) do
 
   create_table "images", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reported_count"
   end
 
   create_table "ratings", force: :cascade do |t|
